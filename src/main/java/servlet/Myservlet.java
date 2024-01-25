af1df53451c43d1a78889832dde2a3934e9b55b8
Mohammed Jaleel <mohjalee@publicisgroupe.net>
2024-01-25 13:22:13 +0530

    jenkins changes for AWS

---
+++ src/main/java/servlet/Myservlet.java
@@ -0,0 +1,25 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/mymethod")
+public class Myservlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><head><title>MyServlet</title></head><body>");
+        out.println("<h2>Method executed successfully!</h2>");
+        out.println("</body></html>");
+    }
+}
